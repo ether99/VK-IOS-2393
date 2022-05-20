be99f2dc1fee9e2d8765d8932bdb2d03895814e6
ether99 <ether8@yandex.ru>
2022-05-20 15:29:44 +0300

    Initial Commit

---
+++ VK IOS 2393/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK IOS 2393
+//
+//  Created by Роберт on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
